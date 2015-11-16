33583d4854e4f6a33567b0b7e3e4ffd1fb8f11b7
sxdxz <gdannyliao@gmail.com>
2015-11-16 18:15:34 +0800

    add annotation

--- app/src/main/java/com/danny/datastruct/graph/UnionFind.java
+++ app/src/main/java/com/danny/datastruct/graph/UnionFind.java
@@ -4,8 +4,19 @@ package com.danny.datastruct.graph;
  * Created by jkl on 15/11/15.
  */
 public abstract class UnionFind {
+    /**
+     * 用于存储节点之间关系的数组，它的意义由具体实现定义
+     */
     int[] id;
+    /**
+     * 存储链接的个数
+     */
     int count;
+
+    /**
+     * 初始化时，每个节点各自为一个链接，所以count=n。另外用id[i] = i表示它自己指向自己
+     * @param n
+     */
     public UnionFind(int n) {
         count = n;
         id = new int[n];
